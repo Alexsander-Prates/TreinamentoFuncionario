ea309005fddc78e4cd939d4489e6ff4fe03acd62
Alexsander-Prates <alexsander.prates@hotmail.com>
2024-04-14 19:27:40 -0300

    estrutura projeto

---
+++ src/main/java/com/alexdevprates/backendstatustreinamentos/BackendstatustreinamentosApplication.java
@@ -0,0 +1,13 @@
+package com.alexdevprates.backendstatustreinamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendstatustreinamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendstatustreinamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexdevprates/backendstatustreinamentos/BackendstatustreinamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexdevprates.backendstatustreinamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendstatustreinamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
