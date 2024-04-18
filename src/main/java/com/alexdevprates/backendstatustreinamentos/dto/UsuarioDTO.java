fac058c793b5546a1b3fd0e4c5caeca597219672
Alexsander-Prates <alexsander.prates@hotmail.com>
2024-04-18 19:34:20 -0300

    Anotações SpringBoot UsuarioDTO

--- src/main/java/com/alexdevprates/backendstatustreinamentos/dto/UsuarioDTO.java
+++ src/main/java/com/alexdevprates/backendstatustreinamentos/dto/UsuarioDTO.java
@@ -1,6 +1,7 @@
 package com.alexdevprates.backendstatustreinamentos.dto;
 
 import com.alexdevprates.backendstatustreinamentos.entity.UsuarioEntity;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import org.springframework.beans.BeanUtils;
 
@@ -17,14 +18,27 @@ public class UsuarioDTO {
 
     private Long id;
 
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min=2,max=50)
+    @Pattern(regexp = "^[A-Z]+(.)*")
     private String nome;
 
+    @NotNull(message = "E-mail é obrigatório")
+    @NotBlank(message = "E-mail é obrigatório")
+    @Email
     private String email;
 
+    @NotNull(message = "Senha obrigatória")
+    @NotBlank(message = "Senha obrigatória")
+    @Size(min = 8, max = 20)
     private String senha;
 
+    @NotNull(message = "Setor é obrigatório")
+    @NotBlank(message = "Setor é obrigatório")
     private String setor;
 
+    @NotNull(message = "Cargo é obrigatório")
+    @NotBlank(message = "Cargo é obrigatório")
     private String cargo;
 
 }
