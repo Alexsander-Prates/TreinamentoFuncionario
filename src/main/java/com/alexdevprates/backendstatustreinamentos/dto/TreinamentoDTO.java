b85f4deaaa32fd0022fd4b6637fb85f3b6497662
Alexsander-Prates <alexsander.prates@hotmail.com>
2024-04-18 19:49:02 -0300

    Anotações SpringBoot TreinamentoDTO

--- src/main/java/com/alexdevprates/backendstatustreinamentos/dto/TreinamentoDTO.java
+++ src/main/java/com/alexdevprates/backendstatustreinamentos/dto/TreinamentoDTO.java
@@ -1,6 +1,10 @@
 package com.alexdevprates.backendstatustreinamentos.dto;
 
 import com.alexdevprates.backendstatustreinamentos.entity.TreinamentoEntity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.beans.BeanUtils;
 
@@ -19,8 +23,13 @@ public class TreinamentoDTO {
 
     private Long id;
 
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min=2,max=50)
+    @Pattern(regexp = "^[A-Z]+(.)*")
     private String nome;
 
+    @NotNull(message = "E-mail é obrigatório")
+    @NotBlank(message = "E-mail é obrigatório")
     private String descricao;
 
     private LocalDateTime inicio;
