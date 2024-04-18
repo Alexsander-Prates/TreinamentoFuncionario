230067db99f5c64307a4db9dd8cd2786e855fb3c
Alexsander-Prates <alexsander.prates@hotmail.com>
2024-04-18 17:20:37 -0300

    Anotações SpringBoot UsuarioEntity

--- src/main/java/com/alexdevprates/backendstatustreinamentos/entity/UsuarioEntity.java
+++ src/main/java/com/alexdevprates/backendstatustreinamentos/entity/UsuarioEntity.java
@@ -1,21 +1,36 @@
+package com.alexdevprates.backendstatustreinamentos.entity;
 
+import java.util.Objects;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name="USUARIO")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioEntity{
 
-    public UsuarioEntity(){
 
-    }
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false, unique = true)
     private String email;
 
-    private Strign senha;
+    @Column(nullable = false)
+    private String senha;
 
+    @Column(nullable = false, unique = true)
     private String setor;
 
+    @Column(nullable = false)
     private String cargo;
 
 
