ce9f91e6b451c56a2a8ab6fb68c7f1b6b9689aee
Alexsander-Prates <alexsander.prates@hotmail.com>
2024-04-18 15:05:31 -0300

    Anotações SpringBoot TreinamentoEntity

--- src/main/java/com/alexdevprates/backendstatustreinamentos/entity/TreinamentoEntity.java
+++ src/main/java/com/alexdevprates/backendstatustreinamentos/entity/TreinamentoEntity.java
@@ -1,21 +1,34 @@
+package com.alexdevprates.backendstatustreinamentos.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="TREINAMENTO")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class TreinamentoEntity{
 
-    public TreinamentoEntity(){
 
-
-    }
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nome:
+    @Column(nullable = false, unique = true)
+    private String nome;
 
     private String descricao;
 
+    @Column(nullable = false)
     private LocalDateTime inicio;
 
-    private LocalDatetime fim;
+    @Column(nullable = false)
+    private LocalDateTime fim;
 
-
-    
 }
