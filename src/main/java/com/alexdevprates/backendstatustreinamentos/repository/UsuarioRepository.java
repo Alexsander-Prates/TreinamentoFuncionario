bb98eda839ff7d6ccae0bb08abc4c72544e9bdfa
Alexsander-Prates <alexsander.prates@hotmail.com>
2024-04-18 21:31:20 -0300

    Correcao UsuarioRepositori ->extends JpaRepository<UsuarioEntity..>

--- src/main/java/com/alexdevprates/backendstatustreinamentos/repository/UsuarioRepository.java
+++ src/main/java/com/alexdevprates/backendstatustreinamentos/repository/UsuarioRepository.java
@@ -1,10 +1,11 @@
 package com.alexdevprates.backendstatustreinamentos.repository;
 
+import com.alexdevprates.backendstatustreinamentos.entity.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<UsuarioRepository, Long> {
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
 
 
 }
