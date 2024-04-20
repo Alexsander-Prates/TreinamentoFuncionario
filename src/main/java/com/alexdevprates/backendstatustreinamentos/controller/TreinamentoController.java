49656c50f8c3f8d52f5469c3392ed8581e747c0b
Alexsander-Prates <alexsander.prates@hotmail.com>
2024-04-19 21:25:36 -0300

    Correcao Base - Class TreinamentoController

--- src/main/java/com/alexdevprates/backendstatustreinamentos/controller/TreinamentoController.java
+++ src/main/java/com/alexdevprates/backendstatustreinamentos/controller/TreinamentoController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TreinamentoController {
 
     @Autowired
-    TreinamentoService treinamentoService;
+    private TreinamentoService treinamentoService;
 
 }
