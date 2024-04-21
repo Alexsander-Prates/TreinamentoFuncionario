e972e543b3cc1596993cdf098388409194f61597
Alexsander-Prates <alexsander.prates@hotmail.com>
2024-04-21 14:09:18 -0300

    Add - Base UsuarioTreinamentoService

---
+++ src/main/java/com/alexdevprates/backendstatustreinamentos/service/UsuarioTreinamentoService.java
@@ -0,0 +1,55 @@
+package com.alexdevprates.backendstatustreinamentos.service;
+
+import com.alexdevprates.backendstatustreinamentos.dto.TreinamentoDTO;
+import com.alexdevprates.backendstatustreinamentos.dto.UsuarioDTO;
+import com.alexdevprates.backendstatustreinamentos.entity.TreinamentoEntity;
+import com.alexdevprates.backendstatustreinamentos.entity.UsuarioEntity;
+import com.alexdevprates.backendstatustreinamentos.entity.UsuarioTreinamentoEntity;
+import com.alexdevprates.backendstatustreinamentos.repository.TreinamentoRepository;
+import com.alexdevprates.backendstatustreinamentos.repository.UsuarioRepository;
+import com.alexdevprates.backendstatustreinamentos.repository.UsuarioTreinamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UsuarioTreinamentoService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private TreinamentoRepository treinamentoRepository;
+
+    @Autowired
+    private UsuarioTreinamentoRepository usuarioTreinamentoRepository;
+
+    public void associarTreinamento(UsuarioDTO usuarioDTO, TreinamentoDTO treinamentoDTO) throws Exception{
+        if((this.usuarioRepository.existsById(usuarioDTO.getId()))
+                &&(this.treinamentoRepository.existsById(treinamentoDTO.getId()))){
+            UsuarioEntity usuarioEntity = this.usuarioRepository.getReferenceById(usuarioDTO.getId());
+            TreinamentoEntity treinamentoEntity = this.treinamentoRepository.getReferenceById(treinamentoDTO.getId());
+
+            UsuarioTreinamentoEntity usuarioTreinamentoEntity = new UsuarioTreinamentoEntity(usuarioEntity,treinamentoEntity);
+            this.usuarioTreinamentoRepository.save(usuarioTreinamentoEntity);
+        } else{
+            throw new Exception("Erro ao associar");
+        }
+    }
+
+    public List<String> listaTreinamentosVinculados() {
+        List<UsuarioTreinamentoEntity> listaTreinamento = this.usuarioTreinamentoRepository.findAll();
+        List<String> listaUsuariosComTreinamentos = new ArrayList<>();
+
+        for (UsuarioTreinamentoEntity usuarioTreinamento : listaTreinamento) {
+            String nomeUsuario = usuarioTreinamento.getUsuarioEntity().getNome();
+            String nomeTreinamento = usuarioTreinamento.getTreinamentoEntity().getNome();
+            String usuarioComTreinamento = nomeUsuario + " - " + nomeTreinamento;
+            listaUsuariosComTreinamentos.add(usuarioComTreinamento);
+        }
+
+        return listaUsuariosComTreinamentos;
+    }
+}
