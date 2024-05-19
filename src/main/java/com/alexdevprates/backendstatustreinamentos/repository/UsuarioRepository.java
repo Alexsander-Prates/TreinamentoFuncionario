b9192cdc02931b84a510c3a133912b02bbaa295c
Alexsander-Prates <alexsander.prates@hotmail.com>
2024-05-19 02:05:49 -0300

    Add base repository usuario login

---
+++ src/main/java/com/alexdevprates/backendstatustreinamentos/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.alexdevprates.backendstatustreinamentos.repository;
+
+import com.alexdevprates.backendstatustreinamentos.entity.UsuarioEntity;
+import com.alexdevprates.backendstatustreinamentos.entity.utils.UsuarioTipo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity,Integer> {
+
+    UserDetails findByLogin(String login);
+}
