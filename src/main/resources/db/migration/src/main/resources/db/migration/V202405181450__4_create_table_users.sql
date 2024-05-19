7faf2e892febdf610e76d0d013b2b4a1cc5128f8
Alexsander-Prates <alexsander.prates@hotmail.com>
2024-05-18 21:32:46 -0300

    Alteracao nome table para usuario

--- src/main/resources/db/migration/src/main/resources/db/migration/V202405181450__4_create_table_users.sql
+++ src/main/resources/db/migration/src/main/resources/db/migration/V202405181450__4_create_table_users.sql
@@ -1,8 +1,8 @@
 -- V4__Create_Tabela_Users.sql
 
-CREATE TABLE users (
+CREATE TABLE usuario (
     id INT PRIMARY KEY NOT NULL,
     login TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    role TEXTE NOT NULL
+    senha TEXT NOT NULL,
+    tipo TEXTE NOT NULL
 );
